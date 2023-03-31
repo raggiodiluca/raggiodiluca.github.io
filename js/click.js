532eef5f5c6c0880dadd744df9f64e10bfe3f2e6
Luca Bessi Aristei <luca.bessi.aristei@gmail.com>
2023-03-31 05:14:18 +0200

    carico media

--- js/click.js
+++ js/click.js
@@ -35,4 +35,19 @@ $(document).ready(function () {
         $currentBoxContainer.find('.box_header').css('opacity', '0');
         $('.box_container').not($currentBoxContainer).css({'pointer-events': 'auto' });
     });
+
+    $(document).on('keydown', function (event) {
+        if (event.key === 'Escape') { // Check if the key is 'Escape'
+            var $currentBoxContainer = $('.box_container.open');
+            if ($currentBoxContainer.length > 0) {
+                var box = $currentBoxContainer.find('.box')[0];
+                box.style.transform = ''; // clear the transform style
+                $('.project_container').scrollTop(0);
+                $currentBoxContainer.toggleClass('open');
+                $currentBoxContainer.css('z-index', '0')
+                $currentBoxContainer.find('.box_header').css('opacity', '0');
+                $('.box_container').not($currentBoxContainer).css({'pointer-events': 'auto' });
+            }
+        }
+    });
 });
