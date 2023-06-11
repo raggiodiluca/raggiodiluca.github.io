088fa27db73136e123996f6350468c00abe77534
Luca Bessi Aristei <luca.bessi.aristei@gmail.com>
2023-06-11 13:17:08 +0200

    fixed infinite rotation with keys and fixed animation opening (I changed the opening scaling animation before so I had to adjust .box_container.open to the same seconds so that it's fluid)

--- js/origamivolanti.js
+++ js/origamivolanti.js
@@ -1,26 +1,23 @@
-var selectedIndex = 0;
+var currentAngle = 0;
 var initialIndex = null;
 
 function rotateCarousel() {
-  var numberOfFaces = $('.open .box_face').length; // add this line to update the number of faces
-  var angle = (selectedIndex - initialIndex) / numberOfFaces * -360;
   var selectedBox = document.querySelector('.open .box');
-  var selectedProject = document.querySelector('.open .box_face:first-child .project_container');
 
-  // Check if the box has a relative with the id #web
+  // Check in which section the opened box is contained, if found the statement is true (has lenght = has parent) 
   var hasWebAncestor = $(selectedBox).parents('#web').length > 0;
   var hasIlluAncestor = $(selectedBox).parents('#illu').length > 0;
   var hasExtraAncestor = $(selectedBox).parents('#extra').length > 0;
   var hasAboutAncestor = $(selectedBox).parents('#about').length > 0;
 
   if (hasWebAncestor) {
-    selectedBox.style.transform = 'translateZ(-30vw) rotateY(' + angle + 'deg) ';
+    selectedBox.style.transform = 'translateZ(-30vw) rotateY(' + currentAngle + 'deg) ';
   } else if (hasIlluAncestor) {
-    selectedBox.style.transform = 'translateZ(-38vw) rotateY(' + angle + 'deg) ';
+    selectedBox.style.transform = 'translateZ(-38vw) rotateY(' + currentAngle + 'deg) ';
   } else if (hasExtraAncestor) {
-    selectedBox.style.transform = 'translateZ(-40vw) rotateY(' + angle + 'deg) ';
+    selectedBox.style.transform = 'translateZ(-40vw) rotateY(' + currentAngle + 'deg) ';
   } else if (hasAboutAncestor) {
-    selectedBox.style.transform = 'translateZ(-25vw) rotateY(' + angle + 'deg) ';
+    selectedBox.style.transform = 'translateZ(-25vw) rotateY(' + currentAngle + 'deg) ';
   }
 
   $('.project_container').animate({
@@ -32,26 +29,28 @@ function rotateCarousel() {
 
 document.addEventListener('keydown', function (event) {
   if ($('.box_container').hasClass('open')) {
+    var numberOfFaces = $('.open .box_face').length; // add this line to update the number of faces
+    var rotationIndex = 360 / numberOfFaces ;
     if (event.key === "ArrowLeft") {
       // Left arrow key
-      selectedIndex--;
+      currentAngle = currentAngle + rotationIndex;
       rotateCarousel();
     } else if (event.key === "ArrowRight") {
       // Right arrow key
-      selectedIndex++;
+      currentAngle = currentAngle - rotationIndex;
       rotateCarousel();
     }
   }
 });
 
 // Click event listener for the box_face elements
-$(document).on('click', '.open .box_face', function (event) {
-  var currentIndex = $(this).index();
-  if (currentIndex > selectedIndex) {
-    selectedIndex = currentIndex;
-    rotateCarousel();
-  } else if (currentIndex < selectedIndex) {
-    selectedIndex = currentIndex;
-    rotateCarousel();
-  }
-});
+// $(document).on('click', '.open .box_face', function (event) {
+//   var currentIndex = $(this).index();
+//   if (currentIndex > selectedIndex) {
+//     selectedIndex = currentIndex;
+//     rotateCarousel();
+//   } else if (currentIndex < selectedIndex) {
+//     selectedIndex = currentIndex;
+//     rotateCarousel();
+//   }
+// });
